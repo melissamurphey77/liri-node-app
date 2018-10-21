18703cc648466a8b9e698e8f703f7bee3b63543e
Melissa Murphey <melissamurphey77@gmail.com>
2018-10-21 00:02:44 -0500

    pushing code so far

--- keys.js
+++ keys.js
@@ -1,8 +1,11 @@
-```js
+
 console.log('this is loaded');
 
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
-```
+
+
+
+
--- liri.js
+++ liri.js
@@ -1,3 +1,61 @@
-```js
+
 require("dotenv").config();
-```
+
+var keys = require('./keys.js');
+
+var Spotify = require('node-spotify-api');
+
+var spotify = new Spotify({
+    id: 
+    , secret: 
+});
+
+var getArtistName = function(artist) {
+    return artist.name;
+
+}
+
+
+var getmeSpotify = function(songName) {
+
+    spotify.search({ type: 'track', query: songName }, function (err, data) {
+        if (err) {
+            console.log('Error occurred: ' + err);
+            return;
+        }
+
+        var songs = data.tracks.items;
+        for (var i=0; i<songs.length; i++) {
+        console.log(i);
+        console.log('artist(s): ' + songs(i).artists.map(
+            getArtistName));
+        console.log('song name: ' + songs(i).name);
+        console.log('preview song: ' + songs(i).preview_url);
+        console.log('album: ' + songs(i).album.name);
+        console.log('------------------------------------------');
+    }
+    
+    }); //end spotify search function
+
+
+}; //end getmeSpotify function
+
+var pick = function (caseData, functionData) {
+    switch (caseData) {
+        case 'spotify-this-song':
+            getmeSpotify(functionData);
+            break;
+        default:
+            console.log('LIRI does not follow your request');
+    }
+}
+
+var runThis = function (argOne, argTwo) {
+    pick(argOne, argTwo);
+
+};
+
+runThis(process.argv[2], process.argv[3]);
+
+
+
